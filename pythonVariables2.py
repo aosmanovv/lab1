f516a353e808b54748d46dfa102f9e70c1928786
aosmanovv <a_osmanov@kbtu.kz>
2025-01-17 22:34:32 +0500

    comments done

---
+++ pythonVariables2.py
@@ -0,0 +1,13 @@
+#correct names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+"""
+2myvar = "John"
+my-var = "John"
+my var = "John"
+uncorrect names
+"""
