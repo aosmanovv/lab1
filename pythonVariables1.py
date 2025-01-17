cfaed8db414d04c119c94a375ab1562a9fbe13be
aosmanovv <a_osmanov@kbtu.kz>
2025-01-17 22:32:35 +0500

    comments done

---
+++ pythonVariables1.py
@@ -0,0 +1,32 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+print(x)
+x = 'John'
+print(x)
+
+a = 4
+print(a)
+A = "Sally"
+print(a)
+#A will not overwrite a
