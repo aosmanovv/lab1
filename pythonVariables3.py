97f64a5a15754e8aa20b4f1803a2c3b48d0fbdab
aosmanovv <a_osmanov@kbtu.kz>
2025-01-17 22:40:44 +0500

    comments done

---
+++ pythonVariables3.py
@@ -0,0 +1,32 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+"""
+Orange
+Banana
+Cherry
+"""
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+"""
+Orange
+Orange
+Orange
+"""
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+"""
+Apple
+Banana
+Cherry
+"""
